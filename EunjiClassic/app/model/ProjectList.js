2c8dc5ba7c32406177d213a54d95a0d0c07a728a
유은지 <ejyoo@nsetec.com>
2021-09-29 15:14:56 +0900

    gird paging, data CRUD 수행

--- EunjiClassic/app/model/ProjectList.js
+++ EunjiClassic/app/model/ProjectList.js
@@ -1,7 +1,7 @@
 Ext.define('EunjiClassic.model.ProjectList', {
     extend: 'EunjiClassic.model.Base',
     fields: [
-        'title','id', 'status', 'description', 'menuTemplate', 
+        'title', 'id', 'status', 'description', 'menuTemplate',
         {name: 'endDate',  type: 'date', dateFormat: 'Y-m-d',
          convert: function(value){
              if(value) value = new Date(value);
@@ -13,6 +13,7 @@ Ext.define('EunjiClassic.model.ProjectList', {
             return value;
         }}
     ],
-    //clientIdProperty: 'ClientId' // parent id와 clientid(server에서 사용)아이디 분리하기 위한 작업
+    //idProperty: 'projectID',
+    //clientIdProperty: 'clientID' // parent id와 clientid(server에서 사용)아이디 분리하기 위한 작업
     
 });

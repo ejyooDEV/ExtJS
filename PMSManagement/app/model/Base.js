8075933878c81328c7de9dd7b6e87a69cc8104a7
ejyoowork <ejyoowork@gmail.com>
2021-11-11 18:59:18 +0900

    no message

---
+++ PMSManagement/app/model/Base.js
@@ -0,0 +1,7 @@
+Ext.define('PMSManagement.model.Base', {
+    extend: 'Ext.data.Model',
+
+    schema: {
+        namespace: 'PMSManagement.model'
+    }
+});
---
+++ PMSManagement/app/model/Personnel.js
@@ -0,0 +1,7 @@
+Ext.define('PMSManagement.model.Personnel', {
+    extend: 'PMSManagement.model.Base',
+
+    fields: [
+        'name', 'email', 'phone'
+    ]
+});
